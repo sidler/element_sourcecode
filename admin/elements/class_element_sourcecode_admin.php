6de87914370cf28e4cff1899d6ee4c84138ee1dd
sidler <sidler@mulchprod.de>
2014-10-17 13:10:43 +0200

    Initial commit

---
+++ admin/elements/class_element_sourcecode_admin.php
@@ -0,0 +1,76 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2014 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*	$Id$                                   *
+********************************************************************************************************/
+
+
+/**
+ * Class to handle the admin-stuff of the sourcecode-element
+ *
+ * @package element_sourcecode
+ * @author sidler@mulchprod.de
+ *
+ * @targetTable element_universal.content_id
+ */
+class class_element_sourcecode_admin extends class_element_admin implements interface_admin_element {
+
+    /**
+     * @var string
+     * @tableColumn element_universal.char1
+     *
+     * @fieldType template
+     * @fieldLabel template
+     *
+     * @fieldTemplateDir /element_sourcecode
+     */
+    private $strChar1;
+
+    /**
+     * @var string
+     * @tableColumn element_universal.text
+     *
+     * @fieldType textarea
+     * @fieldLabel sourcecode_code
+     *
+     * @elementContentTitle
+     */
+    private $intText;
+
+
+
+    /**
+     * @param string $strChar1
+     */
+    public function setStrChar1($strChar1) {
+        $this->strChar1 = $strChar1;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStrChar1() {
+        return $this->strChar1;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIntText() {
+        return $this->intText;
+    }
+
+    /**
+     * @param string $intText
+     */
+    public function setIntText($intText) {
+        $this->intText = $intText;
+    }
+
+
+
+
+}
